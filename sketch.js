65e9179c37f2296ba786851e311cdadb5864c088
Alice Lee <jineel851@gmail.com>
2025-01-27 14:20:32 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+ arc(150, 200, 80, 80, radians(190), radians(340));
+  arc(227, 200, 80, 80, radians(190), radians(340));
+  noFill();
+}
